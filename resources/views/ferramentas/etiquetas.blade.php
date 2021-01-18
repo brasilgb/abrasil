a9143a1e04a1578fd1d7233febeaeb05b6d6fd88
Anderson Brasil <andersonbrasil@outlook.com>
2021-01-18 17:37:27 -0300

    Push 17:37 18-01-2021

--- resources/views/ferramentas/etiquetas.blade.php
+++ resources/views/ferramentas/etiquetas.blade.php
@@ -6,17 +6,27 @@
 
     <title>Hi</title>
     <style>
-        .container{
-            width: 100%;
-            display: inline;
-        }
-        .tags {
-            display: flex;
-            flex-flow: row wrap;
-        }
-        .coluna {
-    width: 20%;
-        }
+       html, body {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+}
+.container {
+    width: 80%;
+    margin: 0 auto;
+    display: flex;
+    flex-wrap: wrap-reverse;
+    flex-direction: row-reverse;
+/* alinha o último item a esquerda */
+    justify-content: flex-end;
+}
+.box {
+    border: 1px solid #000;
+    height: 100px;
+    width:calc(100% / 6);
+    box-sizing: border-box;
+}
     </style>
 </head>
 
@@ -25,10 +35,10 @@
 
 <div class="container">
         @for ($i = $inicial; $i <= $final; $i++)
-        <div class="tags">
-        <div class="coluna">{{ $empresa['empresa'] }}
+        <div class="box">
+        {{ $empresa['empresa'] }}
         {{ $i++ }}
-        {{ $empresa['telefone'] }}</div>
+        {{ $empresa['telefone'] }}
         </div>
         @endfor
 </div>
