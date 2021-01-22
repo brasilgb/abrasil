ed6e78574ae791c5e002f14bc85a2bf7d783fce4
Anderson Brasil <andersonbrasil@outlook.com>
2021-01-22 17:58:19 -0300

    Push 17:58 22-01-2021

--- resources/views/ordens/recibo.blade.php
+++ resources/views/ordens/recibo.blade.php
@@ -6,16 +6,32 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Recibo</title>
-    {{-- <link rel="stylesheet" href="{{ asset('css/relatorios.css') }}"> --}}
+    <link rel="stylesheet" href="{{ asset('css/relatorios.css') }}">
 </head>
 <body>
-<div class="cabecalho">
-    <div class="logo">
+<div class="container">
+            <!-- Ordem de entrega. -->
+<table class="">
+    <tr>
+        <td><img class="logo" src="{{ asset('img/' . $empresa['logo']) }}" title="{{ $empresa['empresa'] }}"></td>
+        <td class="center">
+            {{ $empresa['empresa'] }}<br>
+            {{ 'CNPJ: ' . $empresa['cnpj'] }} -
+            {{ $empresa['endereco'] }},
+            {{ $empresa['bairro'] }}<br>
+            {{ $empresa['cidade'] }},
+            {{ $empresa['uf'] }} <br>
+            {{ $empresa['site'] }}
+            {{ $empresa['email'] }},<br>
+            {{ $empresa['telefone'] }}
+        </td>
+    </tr>
+    <tr>
 
-    </div>
-    <div class="empresa">
 
-    </div>
+    </tr>
+</table>
+
 </div>
 </body>
 </html>
